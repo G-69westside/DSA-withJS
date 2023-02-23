019703aa89497df33512ee197f965f680d8cee22
Tony Maluki <tonymaluki@gmail.com>
2023-02-23 16:10:14 +0300

    Rule book for Big O

--- BigO.js
+++ BigO.js
@@ -22,38 +22,35 @@ function findNemo(array) {
   console.log("nemo was found " + array.length + "times");
 }
 
-findNemo(large) // 0(n) --> It takes linear time to find neom 
+findNemo(large); // 0(n) --> It takes linear time to find neom
 
-
-function funChallenge(input){
-  let a=10; // 0(1)
-  a= 50+3; //0(1)
-  for(let i=0; i< input.length; i++){ // 0(n)
+function funChallenge(input) {
+  let a = 10; // 0(1)
+  a = 50 + 3; //0(1)
+  for (let i = 0; i < input.length; i++) {
+    // 0(n)
     anotherFunction(); // 0(n)
     let stranger = true; // 0(n)
     a++; //0(n)
   }
   return a; //0(1)
-
 } // BIG O(3 + 4n)
 
-
-
 // What is the Big O of the below function? (Hint, you may want to go line by line)
-function anotherFunChallenge(input) { 
+function anotherFunChallenge(input) {
   let a = 5; // O(1)
-  let b = 10;// O(1)
-  let c = 50;// O(1)
-  for (let i = 0; i < input; i++) { // O(n)
+  let b = 10; // O(1)
+  let c = 50; // O(1)
+  for (let i = 0; i < input; i++) {
+    // O(n)
     let x = i + 1; // O(n)
-    let y = i + 2;// O(n)
-    let z = i + 3;// O(n)
-
+    let y = i + 2; // O(n)
+    let z = i + 3; // O(n)
   }
-  for (let j = 0; j < input; j++) { // O(n)
-    let p = j * 2;// O(n)
-    let q = j * 2;// O(n)
+  for (let j = 0; j < input; j++) {
+    // O(n)
+    let p = j * 2; // O(n)
+    let q = j * 2; // O(n)
   }
   let whoAmI = "I don't know"; //O(1)
-}  // 4 + 7n 
- 
+} // 4 + 7n
