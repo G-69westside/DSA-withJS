2052a06bec349f2f95d0663487975a31ad3d57ef
Tony Maluki <tonymaluki@gmail.com>
2023-02-25 16:49:18 +0300

    pairing

---
+++ pairs.js
@@ -0,0 +1,11 @@
+// log all pairs `of array`
+
+const boxes = ["a", "b", "c", "d", "e"];
+function pairs(array) {
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length; j++) {
+      console.log(array[i], array[j]);
+    }
+  }
+}
+pairs(boxes);
