6516b3b5a52f6556b87780348a8fbc55a2e54dbb
Tony Maluki <tonymaluki@gmail.com>
2023-03-26 15:19:21 +0300

    practising

--- linkedlists.js
+++ linkedlists.js
@@ -17,8 +17,19 @@ class LinkedList {
       next: null,
     };
     this.tail = this.head;
-    this.length = 1
+    this.length = 1;
+  }
+  append(value) {
+    const newNode = {
+      value: value,
+      next: null,
+    };
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length++;
   }
 }
 const myLinkedList = new LinkedList(10);
-console.log(myLinkedList)
+myLinkedList.append(5);
+myLinkedList.append(16);
+console.log(myLinkedList);
