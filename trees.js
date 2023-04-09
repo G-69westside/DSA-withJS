aea06623daf8476cf94f34414e883ad8f29372d5
Tony Maluki <tonymaluki@gmail.com>
2023-04-09 20:46:34 +0300

    implementing trees with O(Log N)

---
+++ trees.js
@@ -0,0 +1,76 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+  insert(value) {
+    const newNode = new Node(value);
+    if (this.root === null) {
+      this.root = newNode;
+      return this;
+    } else {
+      let currentNode = this.root;
+      while (true) {
+        if (value < currentNode.value) {
+          if (!currentNode.left) {
+            currentNode.left = newNode;
+            return this;
+          }
+          currentNode = currentNode.left;
+        } else {
+          if (!currentNode.right) {
+            currentNode.right = newNode;
+            return this;
+          }
+          currentNode = currentNode.right;
+        }
+      }
+    }
+  }
+  lookup(value) {
+    if (!this.root) {
+      return console.log(false);
+    }
+    let currentNode = this.root;
+    while (currentNode) {
+      if (value < currentNode.value) {
+        currentNode = currentNode.left;
+      } else if (value > currentNode.value) {
+        currentNode = currentNode.right;
+      } else if (currentNode.value === value) {
+        return console.log(currentNode);
+      }
+    }
+    return console.log(false);
+  }
+}
+// # of nodes = 2^h - 1
+// log nodes  = steps;
+// log 100 = 2
+// 10 ^ 2 = 100
+const tree = new BinarySearchTree();
+tree.insert(9);
+tree.insert(4);
+
+tree.insert(6);
+tree.insert(20);
+tree.insert(170);
+tree.insert(15);
+tree.insert(1);
+tree.lookup(20);
+
+//JSON.stringify(traverse(tree.root));
+//console.log(tree);
+
+function traverse(node) {
+  const tree = { value: node.value };
+  tree.left = node.left === null ? null : traverse(node.left);
+  tree.right = node.right === null ? null : traverse(node.right);
+  return tree;
+}
